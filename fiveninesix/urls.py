0fd7104074b12981ceb1bce74aa998551a37cfee
Eric Brelsford <ebrelsford@gmail.com>
2011-06-15 16:34:44 -0400

    initial commit

---
+++ fiveninesix/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+from django.conf import settings
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
+)
+
+if settings.DEBUG:
+    urlpatterns = patterns('',
+        (r'^' + settings.MEDIA_URL.lstrip('/'), include('appmedia.urls')),
+    ) + urlpatterns

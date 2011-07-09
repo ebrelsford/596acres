554e5b0ae5ee635dc904b9d5ec5a9812cb46a578
Eric Brelsford <ebrelsford@gmail.com>
2011-07-08 20:56:50 -0400

    be more specific about import

--- fiveninesix/contact/cms_app.py
+++ fiveninesix/contact/cms_app.py
@@ -2,7 +2,7 @@ from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
 from django.utils.translation import ugettext_lazy as _
 
-from cms_menu import ContactMenu
+from contact.cms_menu import ContactMenu
 
 class ContactApphook(CMSApp):
     name = _("Contact Apphook")

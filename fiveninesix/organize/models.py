19dc10f23e674259d394a5bca47303969eda5f6d
Eric Brelsford <ebrelsford@gmail.com>
2011-08-02 16:35:18 -0400

    add 'notes' field for organizers, display it

--- fiveninesix/organize/models.py
+++ fiveninesix/organize/models.py
@@ -10,6 +10,7 @@ class Organizer(models.Model):
     phone = models.CharField(max_length=32, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
     url = models.URLField(null=True, blank=True)
+    notes = models.TextField(null=True, blank=True)
 
     lots = models.ManyToManyField(Lot)
     type = models.ForeignKey('OrganizerType')

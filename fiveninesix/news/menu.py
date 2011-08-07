fc8bbe3f1f77ec5e7e78a8a322c8b6d967ac96a5
Eric Brelsford <ebrelsford@gmail.com>
2011-08-07 11:52:59 -0400

    make _get_tag_url visible

--- fiveninesix/news/menu.py
+++ fiveninesix/news/menu.py
@@ -8,15 +8,15 @@ from cms.menu_bases import CMSAttachMenu
 class NewsMenu(CMSAttachMenu):
     name = _("news menu")
 
+    def _get_tag_url(self, tag):
+        return reverse('blog_archive_tagged', kwargs={ 'tag': tag })
+
     def get_nodes(self, request):
         nodes = []
-        nodes.append(NavigationNode(_('Announcements'), _get_tag_url('announcements'), 1))
-        nodes.append(NavigationNode(_('Events'), _get_tag_url('events'), 2))
-        nodes.append(NavigationNode(_('Press'), _get_tag_url('press'), 3))
+        nodes.append(NavigationNode(_('Announcements'), self._get_tag_url('announcements'), 1))
+        nodes.append(NavigationNode(_('Events'), self._get_tag_url('events'), 2))
+        nodes.append(NavigationNode(_('Press'), self._get_tag_url('press'), 3))
         return nodes
-
-    def _get_tag_url(tag):
-        return reverse('blog_archive_tagged', kwargs={ 'tag': tag })
 
 menu_pool.register_menu(NewsMenu)
 

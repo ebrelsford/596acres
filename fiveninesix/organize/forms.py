bd9f47ec1073535f0ac0df9d9a05585dd45c61fc
Eric Brelsford <ebrelsford@gmail.com>
2011-08-18 10:52:39 -0400

    email notifications when organizers added

--- fiveninesix/organize/forms.py
+++ fiveninesix/organize/forms.py
@@ -1,10 +1,32 @@
+from django.core.mail import mail_managers
+from django.core.urlresolvers import reverse
 from django.forms import ModelForm, MultipleHiddenInput, ModelMultipleChoiceField
 
 from lots.models import Lot
 from models import Organizer
+from settings import BASE_URL
 
 class OrganizerForm(ModelForm):
     lots = ModelMultipleChoiceField(label='lots', queryset=Lot.objects.all(), widget=MultipleHiddenInput())
 
     class Meta:
         model = Organizer
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        organizer = super(self.__class__, self).save()
+        self._send_notification(organizer)
+
+    def _send_notification(self, organizer):
+        lots_urls = [BASE_URL + reverse('lots.views.details', args=(l.bbl,)) for l in organizer.lots.all()]
+        message = """Neat! A new organizer was created on 596acres.org.
+
+Details:
+name: %s
+type: %s
+phone: %s
+email: %s
+url: %s
+lots: %s
+""" % (organizer.name, organizer.type.name, organizer.phone, organizer.email, organizer.url, ', '.join(lots_urls),)
+
+        mail_managers('A new organizer was created on 596acres.org', message)

73ad5aca301a8fe9438bc51b090900f8fecd9424
Eric Brelsford <ebrelsford@gmail.com>
2012-02-21 10:22:09 -0500

    recently added dashboardmodule

--- fiveninesix/dashboard.py
+++ fiveninesix/dashboard.py
@@ -1,8 +1,31 @@
-from django.utils.translation import ugettext_lazy as _
-from django.core.urlresolvers import reverse
+from datetime import date, timedelta
 
-from admin_tools.dashboard import modules, Dashboard, AppIndexDashboard
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
+
+from admin_tools.dashboard import modules, Dashboard
 from admin_tools.utils import get_admin_site_name
+
+from fiveninesix.lots.models import Review
+from fiveninesix.organize.models import Note, Organizer, Picture, Watcher
+
+class RecentlyAddedDashboardModule(modules.LinkList):
+    def __init__(self, title='The Past 7 Days', **kwargs):
+        super(RecentlyAddedDashboardModule, self).__init__(title, **kwargs)
+
+        added_cutoff = date.today() - timedelta(days=7)
+        for model in (Note, Organizer, Picture, Review, Watcher,):
+            count = model.objects.filter(added__gt=added_cutoff).count()
+            pluralize = 's'
+            if count == 1:
+                pluralize = ''
+
+            url = reverse('admin:%s_%s_changelist' % (model.__module__.split('.')[0], model.__name__.lower(),))
+
+            self.children.append({
+                'title': '%d %s%s' % (count, model.__name__, pluralize),
+                'url': url + '?added__gte=%s' % added_cutoff.strftime('%Y-%m-%d'),
+            })
 
 class CommonTasksDashboardModule(modules.DashboardModule):
     
@@ -19,6 +42,7 @@ class CustomIndexDashboard(Dashboard):
         site_name = get_admin_site_name(context)
 
         self.children.append(CommonTasksDashboardModule())
+        self.children.append(RecentlyAddedDashboardModule())
 
         # append an app list module for "Applications"
         self.children.append(modules.AppList(

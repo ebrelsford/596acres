7af028446404c76c891487bf5b3c035bc6dbe9bd
Eric Brelsford <ebrelsford@gmail.com>
2012-04-03 11:22:01 -0400

    review change column

--- fiveninesix/lots/admin.py
+++ fiveninesix/lots/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
 
 from fiveninesix.admin import LotRelatedModelAdmin
 from models import Lot, Owner, OwnerType, Review
@@ -21,13 +22,18 @@ class OwnerTypeAdmin(admin.ModelAdmin):
 
 class ReviewAdmin(LotRelatedModelAdmin):
     search_fields = ('lot__bbl',)
-    list_display = ('id', 'in_use', 'accessible', 'should_be_imported', 'imported', 'actual_use', 'reviewer', 'added', 'acres', 'view_lot', 'view_in_oasis',)
+    list_display = ('id', 'in_use', 'accessible', 'should_be_imported', 'imported', 'actual_use', 'reviewer', 'added', 'acres', 'view_lot', 'view_in_oasis', 'change_review',)
     list_editable = ('should_be_imported',)
     list_filter = ('should_be_imported', 'imported', 'in_use', 'accessible', 'needs_further_review', 'added', 'actual_use',)
 
     def acres(self, obj):
         return obj.lot.area_acres
     acres.admin_order_field = 'lot__area_acres'
+
+    def change_review(self, obj):
+        lot = obj.lot
+        return '<a target="_blank" href="%s">%s</a>' % (reverse('lots.views.add_review', kwargs={ 'bbl': lot.bbl }), 'change review',)
+    change_review.allow_tags = True
 
 admin.site.register(Lot, LotAdmin)
 admin.site.register(Owner, OwnerAdmin)

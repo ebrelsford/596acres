9a9e10b4f255de1c759fecd08d7be28a03fb35f0
Eric Brelsford <ebrelsford@gmail.com>
2012-04-04 10:42:00 -0400

    events views

--- fiveninesix/events/urls_app.py
+++ fiveninesix/events/urls_app.py
@@ -5,9 +5,10 @@ from django.views.generic import DetailView, ListView
 
 from models import Event
 
+# TODO month view https://github.com/justinlilly/django-gencal or https://github.com/visualspace/django-agenda ?
 urlpatterns = patterns('',
     url(r'^$', ListView.as_view(
-        queryset=Event.objects.filter(status='active', start__gte=datetime.now()),
+        queryset=Event.objects.filter(status='active', start__gte=datetime.now()).order_by('start'),
     ), name='events_event_list'),
     url(r'^(?P<pk>\d+)/$', DetailView.as_view(
         model=Event,

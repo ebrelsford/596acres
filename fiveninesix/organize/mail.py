feed6741688777851757bf28f064d85159e244a9
Eric Brelsford <ebrelsford@gmail.com>
2012-04-18 18:45:09 -0400

    send organizers email from a non-admin email address

--- fiveninesix/organize/mail.py
+++ fiveninesix/organize/mail.py
@@ -7,7 +7,7 @@ import settings
 def mail_organizers(subject, message, fail_silently=False, connection=None, html_message=None):             
     """Sends a message to all organizers."""
     organizers = Organizer.objects.filter(email__isnull=False).exclude(email='')
-    _mail_multiple(subject, message, [o.email for o in organizers], fail_silently=fail_silently, connection=connection, html_message=html_message)
+    _mail_multiple(subject, message, [o.email for o in organizers], fail_silently=fail_silently, connection=connection, html_message=html_message, from_email=settings.ORGANIZERS_EMAIL)
 
 def mail_watchers(lot, subject, message, fail_silently=False, connection=None, html_message=None):             
     """Sends a message to watchers of a given lot."""
@@ -26,12 +26,11 @@ def _mail_multiple_personalized(subject, messages, fail_silently=False, connecti
     for email, message in messages.items():
         _mail_multiple(subject, message, [email], fail_silently=fail_silently, connection=connection, html_message=html_message)
 
-def _mail_multiple(subject, message, email_addresses, fail_silently=False, connection=None, html_message=None):
+def _mail_multiple(subject, message, email_addresses, fail_silently=False, connection=None, html_message=None, from_email=settings.SERVER_EMAIL):
     """Sends a message to multiple email addresses. Based on django.core.mail.mail_admins()"""
     for email_address in email_addresses:
-        mail = EmailMultiAlternatives(u'%s%s' % (settings.EMAIL_SUBJECT_PREFIX, subject), message, from_email=settings.SERVER_EMAIL,
+        mail = EmailMultiAlternatives(u'%s%s' % (settings.EMAIL_SUBJECT_PREFIX, subject), message, from_email=from_email,
                                       to=[email_address], connection=connection, bcc=settings.MANAGERS)          
         if html_message:
             mail.attach_alternative(html_message, 'text/html')
         mail.send(fail_silently=fail_silently)
-

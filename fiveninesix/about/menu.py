ec62e699b7c530822b24b060a125c06619ff2f47
Eric Brelsford <ebrelsford@gmail.com>
2012-04-23 11:52:35 -0400

    previous events in about menu

---
+++ fiveninesix/about/menu.py
@@ -0,0 +1,18 @@
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
+
+from menus.base import NavigationNode
+from menus.menu_pool import menu_pool
+from cms.menu_bases import CMSAttachMenu
+
+class AboutMenu(CMSAttachMenu):
+    name = _("about menu")
+
+    def get_nodes(self, request):
+        nodes = [
+            NavigationNode(_('Previous Events'),
+                           reverse('events_event_list_past'), 1),
+        ]
+        return nodes
+
+menu_pool.register_menu(AboutMenu)

1af4e2fcb83b6d22ededd0c3ea545032e1b2c437
Eric Brelsford <ebrelsford@gmail.com>
2012-04-23 12:51:05 -0400

    remove contact menu in favor of getinvolved menu

--- fiveninesix/contact/cms_app.py
+++ fiveninesix/contact/cms_app.py
@@ -2,11 +2,8 @@ from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
 from django.utils.translation import ugettext_lazy as _
 
-#from contact.menu import ContactMenu
-
 class ContactApphook(CMSApp):
     name = _("Contact Apphook")
     urls = ('contact.urls',)
-    #menus = (ContactMenu,)
 
 apphook_pool.register(ContactApphook)

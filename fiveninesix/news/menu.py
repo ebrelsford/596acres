2a60ff921997ba7be7c3ad6c275002cf7fb8fc66
Eric Brelsford <ebrelsford@gmail.com>
2012-05-03 13:34:58 -0400

    tool updates

--- fiveninesix/news/menu.py
+++ fiveninesix/news/menu.py
@@ -15,8 +15,7 @@ class NewsMenu(CMSAttachMenu):
         nodes = []
         nodes.append(NavigationNode(_('Announcements'), self._get_tag_url('announcements'), 2))
         nodes.append(NavigationNode(_('596 Acres Press'), self._get_tag_url('596 acres press'), 2))
-        nodes.append(NavigationNode(_('Of note'), self._get_tag_url('of note'), 1))
-        nodes.append(NavigationNode(_('Site Updates'), self._get_tag_url('site updates'), 1))
+        nodes.append(NavigationNode(_('Tool Updates'), self._get_tag_url('tool updates'), 1))
         return nodes
 
 menu_pool.register_menu(NewsMenu)

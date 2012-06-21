f5d6cfb7c97fd357e94ddb2cebcaf75a4485da25
Eric Brelsford <ebrelsford@gmail.com>
2012-06-21 12:17:19 -0400

    cache events views

--- fiveninesix/events/urls_app.py
+++ fiveninesix/events/urls_app.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, url
+from django.views.decorators.cache import cache_page
 from django.views.generic import DetailView
 
 from models import Event
@@ -6,8 +7,15 @@ from views import EventListView
 
 # TODO month view https://github.com/justinlilly/django-gencal or https://github.com/visualspace/django-agenda ?
 urlpatterns = patterns('',
-    url(r'^$', EventListView.as_view(), name='events_event_list'),
-    url(r'^past/$', EventListView.as_view(), kwargs={'past': True}, name='events_event_list_past'),
+    url(r'^$', 
+        cache_page(60 * 60)(EventListView.as_view()),
+        name='events_event_list'
+    ),
+    url(r'^past/$',
+        cache_page(60 * 60)(EventListView.as_view()),
+        kwargs={'past': True},
+        name='events_event_list_past'
+    ),
     url(r'^(?P<pk>\d+)/$', DetailView.as_view(
         model=Event,
     ), name='events_event_detail'),

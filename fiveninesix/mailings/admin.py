ff19453b9b172f8a7115fa4992be3be5aaab38e4
Eric Brelsford <ebrelsford@gmail.com>
2012-07-17 15:13:26 -0400

    last_checked on admin

--- fiveninesix/mailings/admin.py
+++ fiveninesix/mailings/admin.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from mailings.models import DaysAfterAddedMailing, DeliveryRecord, WatcherThresholdMailing
 
 class DaysAfterAddedMailingAdmin(admin.ModelAdmin):
-    list_display = ('name', 'days_after_added',)
+    list_display = ('name', 'last_checked', 'days_after_added',)
 
 class WatcherThresholdMailingAdmin(admin.ModelAdmin):
-    list_display = ('name', 'number_of_watchers',)
+    list_display = ('name', 'last_checked', 'number_of_watchers',)
 
 class DeliveryRecordAdmin(admin.ModelAdmin):
     list_display = ('receiver_object', 'mailing', 'sent', 'recorded',)

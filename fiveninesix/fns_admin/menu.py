87745a6ddd4b860ce5d0eab3a41dc9bc79598c81
Eric Brelsford <ebrelsford@gmail.com>
2012-07-19 14:27:14 -0400

    backend in menu

--- fiveninesix/fns_admin/menu.py
+++ fiveninesix/fns_admin/menu.py
@@ -10,17 +10,26 @@ class FNSAdminMenu(CMSAttachMenu):
 
     def get_nodes(self, request):
         nodes = []
-        nodes.append(NavigationNode(_('Email All Organizers'), '/fnsadmin/organizers/mail/', 1))
-        nodes.append(NavigationNode(_('Add News'), '/admin/cmsplugin_blog/entry/add/', 2))
+
+        nodes.append(NavigationNode(_('Backend'), 
+                                    reverse('admin:index'), 1))
+
+        nodes.append(NavigationNode(_('Email All Organizers'),
+                                    '/fnsadmin/organizers/mail/', 1))
+
+        nodes.append(NavigationNode(_('Add News'),
+                                    '/admin/cmsplugin_blog/entry/add/', 2))
 
         for borough in ['Manhattan', 'Queens', 'Bronx', 'Staten Island']:
             nodes.append(
                 NavigationNode(
                     'Review %s Lots' % borough,
-                    reverse('fns_admin.views.review_lots', kwargs={ 'borough': borough }),
+                    reverse('fns_admin.views.review_lots',
+                            kwargs={ 'borough': borough }),
                     3
                 )
             )
+
         return nodes
 
 menu_pool.register_menu(FNSAdminMenu)

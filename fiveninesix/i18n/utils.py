57114b87f357d5b0558949c9003dcbd8144d734f
Eric Brelsford <ebrelsford@gmail.com>
2012-08-04 23:44:27 -0400

    default language

--- fiveninesix/i18n/utils.py
+++ fiveninesix/i18n/utils.py
@@ -1,4 +1,12 @@
+from django.conf import settings
 from django.utils import translation
 
-def language_namespaced_view_name(view_name):
-    return '%s:%s' % (translation.get_language(), view_name)
+def language_namespaced_view_name(view_name, lang=None):
+    if not lang:
+        try:
+            lang = translation.get_language().split('-')[0]
+        except Exception:
+            pass
+    if not lang:
+        lang = settings.LANGUAGE_CODE
+    return '%s:%s' % (lang, view_name)

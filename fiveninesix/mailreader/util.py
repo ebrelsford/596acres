b6bef38603de41cca666135f0584422049345aa2
Eric Brelsford <ebrelsford@gmail.com>
2012-09-20 16:50:39 -0400

    accept more types of payloads

--- fiveninesix/mailreader/util.py
+++ fiveninesix/mailreader/util.py
@@ -1,29 +1,49 @@
+from collections import Iterable
 from email import message_from_string
 from imapclient import IMAPClient
 
 from django.conf import settings
 
-def get_mail():
+def login():
+    """
+    Log in to the mailbox we will read from.
+    """
     imap = IMAPClient(settings.MAILREADER_HOST, use_uid=True)
     imap.login(settings.MAILREADER_HOST_USER, settings.MAILREADER_HOST_PASSWORD)
     imap.select_folder('INBOX')
+    return imap
 
+def get_messages(imap):
+    """
+    Get messages from the given server.
+    """
     messages = imap.search(['NOT SEEN'])
-    response = imap.fetch(messages, ['RFC822'])
+    return imap.fetch(messages, ['RFC822'])
+
+def consolidate_payloads(payload):
+    """
+    Get text from a message's payload.
+    """
+    if isinstance(payload, str):
+        return (payload,)
+    elif isinstance(payload, Iterable):
+        payloads = []
+        for p in payload:
+            payloads += consolidate_payloads(p.get_payload())
+        return payloads
+    elif not payload.is_multipart() and payload.get_content_type() == 'text/plain':
+        return payload.get_payload().strip()
+
+def get_mail():
+    imap = login()
+    response = get_messages(imap)
 
     mail = []
-
     for uid, message in response.items():
         parsed = message_from_string(message['RFC822'])
-        payloads = []
-        for payload in parsed.get_payload():
-            if isinstance(payload, str):
-                payloads.append(payload)
-            elif not payload.is_multipart() and payload.get_content_type() == 'text/plain':
-                payloads.append(payload.get_payload().strip())
         mail.append({
             'from_address': parsed['From'],
-            'payloads': payloads,
+            'payloads': consolidate_payloads(parsed.get_payload()),
             'subject': parsed['Subject'],
             'to_address': parsed['To'],
         })
